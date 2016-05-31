4dd100857757f76bf5d391ed5b7f555fb74c8020
floodliu <floodliu@gmail.com>
2016-05-31 14:55:23 +0800

    change some comments.

--- smsSamples/java-sms-send/src/main/java/com/example/App.java
+++ smsSamples/java-sms-send/src/main/java/com/example/App.java
@@ -19,7 +19,7 @@ public class App {
     private static Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        // 第一步: 配置AK/SK, 创建SmsClient。
+        // 配置AK/SK, 创建SmsClient。
         final String AK = "";
         final String SK = "";
 
@@ -27,18 +27,18 @@ public class App {
         configuration.setCredentials(new DefaultBceCredentials(AK, SK));
         SmsClient client = new SmsClient(configuration);
 
-        // 第二步: 配置短信模板, 模板中参数的值, 接收端手机号码。
+        // 配置短信模板, 模板中参数的值, 接收端手机号码。
         final String templateId = "smsTpl:e7476122a1c24e37b3b0de19d04ae900";  // 模板内容: 您的验证码是${code}
         final String contentVar = "{\"code\": \"12345\"}";  // 配置模板参数的值, JSON格式。
         final List<String> phoneNumbers = Arrays.asList("手机号码1", "手机号码2");  // 支持多个接收端手机号码
 
-        // 第三步: 创建"发送短信"的请求
+        // 创建"发送短信"的请求
         SendMessageRequest request = new SendMessageRequest();
         request.setTemplateId(templateId);
         request.setContentVar(contentVar);
         request.setReceiver(phoneNumbers);
 
-        // 第四步: 发送短信
+        // 发送短信
         SendMessageResponse response = client.sendMessage(request);
         logger.info(response.getMessageId());
     }
