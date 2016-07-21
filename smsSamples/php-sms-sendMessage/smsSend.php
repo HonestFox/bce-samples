96e7182922e50c4aeda4aaf7616fb7c24a96bfa4
floodliu <floodliu@gmail.com>
2016-07-21 20:53:03 +0800

    reformat smsSend.php

--- smsSamples/php-sms-sendMessage/smsSend.php
+++ smsSamples/php-sms-sendMessage/smsSend.php
@@ -3,26 +3,26 @@
 date_default_timezone_set('UTC');
 $AK = "";  // 填写AK（AccessKeyId）
 $SK = "";  // 填写SK（SecretAccessKey）
-$timestamp = date("Y-m-d")."T".date("H:i:s")."Z";
+$timestamp = date("Y-m-d") . "T" . date("H:i:s") . "Z";
 $expirationPeriodInSeconds = "3600";  // 设置签名过期时间（单位：秒）
 
 // 计算签名
-$authStringPrefix = "bce-auth-v1"."/".$AK."/".$timestamp."/".$expirationPeriodInSeconds;
+$authStringPrefix = "bce-auth-v1" . "/" . $AK . "/" . $timestamp . "/" . $expirationPeriodInSeconds;
 $SigningKey = hash_hmac('SHA256', $authStringPrefix, $SK);
-$CanonicalHeaders1 = "host;"."x-bce-date";
-$CanonicalHeaders2 = "host:sms.bj.baidubce.com\n"."x-bce-date:".urlencode($timestamp);//
-print "CanonicalHeaders2:".$CanonicalHeaders2."\n";
+$CanonicalHeaders1 = "host;" . "x-bce-date";
+$CanonicalHeaders2 = "host:sms.bj.baidubce.com\n" . "x-bce-date:" . urlencode($timestamp);
+print "CanonicalHeaders2:" . $CanonicalHeaders2 . "\n";
 
 $CanonicalString = "";
 $CanonicalURI = "/v1/message";
-$CanonicalRequest = "POST\n".$CanonicalURI."\n".$CanonicalString."\n".$CanonicalHeaders2;
-print "CanonicalRequest:".$CanonicalRequest."\n";
+$CanonicalRequest = "POST\n" . $CanonicalURI . "\n" . $CanonicalString . "\n" . $CanonicalHeaders2;
+print "CanonicalRequest:" . $CanonicalRequest . "\n";
 
 $Signature = hash_hmac('SHA256', $CanonicalRequest, $SigningKey);
-print "Signature:".$Signature."\n";
+print "Signature:" . $Signature . "\n";
 
-$Authorization = "bce-auth-v1/{$AK}/".$timestamp."/{$expirationPeriodInSeconds}/{$CanonicalHeaders1}/{$Signature}";
-print "Authorization:".$Authorization."\n";
+$Authorization = "bce-auth-v1/{$AK}/" . $timestamp . "/{$expirationPeriodInSeconds}/{$CanonicalHeaders1}/{$Signature}";
+print "Authorization:" . $Authorization . "\n";
 
 // 设置模板ID、模板参数值、接收者的手机号码。
 $url = "http://sms.bj.baidubce.com/v1/message";
@@ -44,7 +44,7 @@ $curlp = curl_init();
 curl_setopt($curlp, CURLOPT_URL, $url);
 curl_setopt($curlp, CURLOPT_HTTPHEADER, $head);
 curl_setopt($curlp, CURLOPT_POSTFIELDS, $data_string);
-if(!empty($data)) {
+if (!empty($data)) {
     curl_setopt($curlp, CURLOPT_POST, 1);
 }
 
@@ -52,6 +52,5 @@ if(!empty($data)) {
 curl_setopt($curlp, CURLOPT_RETURNTRANSFER, 1);
 $output = curl_exec($curlp);
 curl_close($curlp);
-echo $output;
-print "\n";
+print $output . "\n";
 ?>
