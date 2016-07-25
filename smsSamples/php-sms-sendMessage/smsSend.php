9d418ca3654d1940ae553404ed549575fe5cc6d3
floodliu <floodliu@gmail.com>
2016-07-25 13:38:41 +0800

    encapsulate authorization into a function.

--- smsSamples/php-sms-sendMessage/smsSend.php
+++ smsSamples/php-sms-sendMessage/smsSend.php
@@ -1,28 +1,49 @@
 <?php
-// 配置签名信息
-date_default_timezone_set('UTC');
-$AK = "";  // 填写AK（AccessKeyId）
-$SK = "";  // 填写SK（SecretAccessKey）
-$timestamp = date("Y-m-d") . "T" . date("H:i:s") . "Z";
-$expirationPeriodInSeconds = "3600";  // 设置签名过期时间（单位：秒）
 
-// 计算签名
-$authStringPrefix = "bce-auth-v1" . "/" . $AK . "/" . $timestamp . "/" . $expirationPeriodInSeconds;
-$SigningKey = hash_hmac('SHA256', $authStringPrefix, $SK);
-$CanonicalHeaders1 = "host;" . "x-bce-date";
-$CanonicalHeaders2 = "host:sms.bj.baidubce.com\n" . "x-bce-date:" . urlencode($timestamp);
-print "CanonicalHeaders2:" . $CanonicalHeaders2 . "\n";
+// 生成HTTP请求头（内含认证信息）
+function createHeadWithAuthorization($ak, $sk, $expirationPeriodInSeconds, $method, $uri="/v1/message", $host='sms.bj.baidubce.com') {
+    date_default_timezone_set("UTC");
+    $timestamp = date("Y-m-d") . "T" . date("H:i:s") . "Z";
 
-$CanonicalString = "";
-$CanonicalURI = "/v1/message";
-$CanonicalRequest = "POST\n" . $CanonicalURI . "\n" . $CanonicalString . "\n" . $CanonicalHeaders2;
-print "CanonicalRequest:" . $CanonicalRequest . "\n";
+    // 生成认证字符串
+    $authStringPrefix = "bce-auth-v1" . "/" . $ak . "/" . $timestamp . "/" . $expirationPeriodInSeconds;
+    $signingKey = hash_hmac("SHA256", $authStringPrefix, $sk);
+    $canonicalHeaders1 = "host;" . "x-bce-date";
+    $canonicalHeaders2 = "host:" . $host . "\n" . "x-bce-date:" . urlencode($timestamp);
+    print("canonicalHeaders2: " . $canonicalHeaders2 . "\n");
 
-$Signature = hash_hmac('SHA256', $CanonicalRequest, $SigningKey);
-print "Signature:" . $Signature . "\n";
+    $method = strtoupper($method);
+    $canonicalRequest = $method . "\n" . $uri . "\n\n" . $canonicalHeaders2;
+    print("canonicalRequest: " . $canonicalRequest . "\n");
 
-$Authorization = "bce-auth-v1/{$AK}/" . $timestamp . "/{$expirationPeriodInSeconds}/{$CanonicalHeaders1}/{$Signature}";
-print "Authorization:" . $Authorization . "\n";
+    $signature = hash_hmac("SHA256", $canonicalRequest, $signingKey);
+    print("signature: " . $signature . "\n");
+
+    $authorization = "bce-auth-v1/{$ak}/" . $timestamp . "/{$expirationPeriodInSeconds}/{$canonicalHeaders1}/{$signature}";
+    print("authorization: " . $authorization . "\n");
+
+    // 生成HTTP请求头
+    if ($method == "POST" or $method == "PUT") {
+        return array(
+            "Content-Type: application/json",
+            "Authorization: {$authorization}",
+            "x-bce-date: {$timestamp}",
+            "x-bce-content-sha256: {$signingKey}"
+        );
+    } else {
+        return array(
+            "Content-Type: application/json",
+            "Authorization: {$authorization}",
+            "x-bce-date: {$timestamp}"
+        );
+    }
+}
+
+$AK = "";  // AccessKeyID
+$SK = "";  // SecretAccessKey
+$expirationPeriodInSeconds = 3600;
+$method = "POST";
+$head = createHeadWithAuthorization($AK, $SK, $expirationPeriodInSeconds, $method);
 
 // 设置模板ID、模板参数值、接收者的手机号码。
 $url = "http://sms.bj.baidubce.com/v1/message";
@@ -33,13 +54,7 @@ $data = array(
     );
 $data_string = json_encode($data);
 
-// 构造HTTP请求
-$head =  array(
-    "Content-Type: application/json",
-    "Authorization: {$Authorization}",
-    "x-bce-date: {$timestamp}",
-    "x-bce-content-sha256: {$SigningKey}"
-    );
+// 设置HTTP请求的Header和Body
 $curlp = curl_init();
 curl_setopt($curlp, CURLOPT_URL, $url);
 curl_setopt($curlp, CURLOPT_HTTPHEADER, $head);
@@ -50,7 +65,7 @@ if (!empty($data)) {
 
 // 发送短信
 curl_setopt($curlp, CURLOPT_RETURNTRANSFER, 1);
-$output = curl_exec($curlp);
+$response = curl_exec($curlp);
 curl_close($curlp);
-print $output . "\n";
+print $response . "\n";
 ?>
