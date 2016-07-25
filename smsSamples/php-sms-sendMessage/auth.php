00ba4244d9feeebc5f9d29537b961ab8ce3f62e5
floodliu <floodliu@gmail.com>
2016-07-25 17:56:31 +0800

    move code of authorization to a single file.

--- smsSamples/php-sms-sendMessage/auth.php
+++ smsSamples/php-sms-sendMessage/auth.php
@@ -1,7 +1,7 @@
 <?php
 
 // 生成HTTP请求头（内含认证信息）
-function createHeadWithAuthorization($ak, $sk, $expirationPeriodInSeconds, $method, $uri="/v1/message", $host='sms.bj.baidubce.com') {
+function createHeadWithAuthorization($ak, $sk, $expirationPeriodInSeconds, $method, $uri, $host) {
     date_default_timezone_set("UTC");
     $timestamp = date("Y-m-d") . "T" . date("H:i:s") . "Z";
 
@@ -13,7 +13,8 @@ function createHeadWithAuthorization($ak, $sk, $expirationPeriodInSeconds, $meth
     print("canonicalHeaders2: " . $canonicalHeaders2 . "\n");
 
     $method = strtoupper($method);
-    $canonicalRequest = $method . "\n" . $uri . "\n\n" . $canonicalHeaders2;
+    $canonicalQueryString = "";
+    $canonicalRequest = $method . "\n" . $uri . "\n" . $canonicalQueryString . "\n" . $canonicalHeaders2;
     print("canonicalRequest: " . $canonicalRequest . "\n");
 
     $signature = hash_hmac("SHA256", $canonicalRequest, $signingKey);
@@ -39,33 +40,4 @@ function createHeadWithAuthorization($ak, $sk, $expirationPeriodInSeconds, $meth
     }
 }
 
-$AK = "";  // AccessKeyID
-$SK = "";  // SecretAccessKey
-$expirationPeriodInSeconds = 3600;
-$method = "POST";
-$head = createHeadWithAuthorization($AK, $SK, $expirationPeriodInSeconds, $method);
-
-// 设置模板ID、模板参数值、接收者的手机号码。
-$url = "http://sms.bj.baidubce.com/v1/message";
-$data = array(
-    'templateId' => 'smsTpl:e7476122a1c24e37b3b0de19d04ae900',  // 内置短信模板，内容为：您的验证码是${code}
-    'contentVar' => json_encode(array('code' => '12345')),  // 设置模板中的参数值
-    'receiver' => array('手机号码1', '手机号码2')  // 支持多个手机号码
-    );
-$data_string = json_encode($data);
-
-// 设置HTTP请求的Header和Body
-$curlp = curl_init();
-curl_setopt($curlp, CURLOPT_URL, $url);
-curl_setopt($curlp, CURLOPT_HTTPHEADER, $head);
-curl_setopt($curlp, CURLOPT_POSTFIELDS, $data_string);
-if (!empty($data)) {
-    curl_setopt($curlp, CURLOPT_POST, 1);
-}
-
-// 发送短信
-curl_setopt($curlp, CURLOPT_RETURNTRANSFER, 1);
-$response = curl_exec($curlp);
-curl_close($curlp);
-print $response . "\n";
 ?>
---
+++ smsSamples/php-sms-sendMessage/sendMessage.php
@@ -0,0 +1,38 @@
+<?php
+
+require "auth.php";
+
+// 设置AK/SK，生成认证信息。
+$AK = "";  // AccessKeyID
+$SK = "";  // SecretAccessKey
+$expirationPeriodInSeconds = 3600;
+$method = "POST";
+$uri = "/v1/message";
+$host = "sms.bj.baidubce.com";
+$head = createHeadWithAuthorization($AK, $SK, $expirationPeriodInSeconds, $method, $uri, $host);
+
+// 设置模板ID、模板参数值、接收者的手机号码。
+$body = array(
+    'templateId' => 'smsTpl:e7476122a1c24e37b3b0de19d04ae900',  // 内置短信模板，内容为：您的验证码是${code}
+    'contentVar' => json_encode(array('code' => '12345')),  // 设置模板中的参数值
+    'receiver' => array('手机号码1', '手机号码2')  // 支持多个手机号码
+    );
+$bodyString = json_encode($body);
+
+// 设置HTTP请求的Header和Body
+$curlp = curl_init();
+$url = "http://" . $host . $uri;
+curl_setopt($curlp, CURLOPT_URL, $url);
+curl_setopt($curlp, CURLOPT_HTTPHEADER, $head);
+if (!empty($body)) {
+    curl_setopt($curlp, CURLOPT_POST, 1);
+    curl_setopt($curlp, CURLOPT_POSTFIELDS, $bodyString);
+}
+
+// 发送短信
+curl_setopt($curlp, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($curlp);
+curl_close($curlp);
+print($response . "\n");
+
+?>
