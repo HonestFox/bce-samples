8fb3e4b9eb431e1b1e0c01a7dea18e19f898687b
floodliu <floodliu@gmail.com>
2016-08-22 17:22:44 +0800

    fix a code error of java-ses-setFeedback.

--- sesSamples/java-ses-setFeedback/src/main/java/com/example/App.java
+++ sesSamples/java-ses-setFeedback/src/main/java/com/example/App.java
@@ -10,7 +10,7 @@ import com.baidubce.services.ses.model.SetFeedbackRequest;
  *
  * 注意: 设置了退信通知的接收邮箱之后, 在"管理控制台"无法查看任何设置信息, 需要调用SDK中的getFeedback接口才能查看。
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
@@ -23,7 +23,7 @@ public class App
 
         SetFeedbackRequest request = new SetFeedbackRequest();
         request.setType(2);  // 退信通知
-        request.setEmail();  // 接收通知的邮箱地址, 比如: xyz@abc.com
+        request.setEmail("");  // 接收通知的邮箱地址, 比如: xyz@abc.com
         request.setEnabled(true);  // 使上面的邮箱地址接收(true)/不接收(false)退信通知
         client.setFeedback(request);
     }
