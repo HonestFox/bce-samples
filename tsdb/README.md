5ed471f021eba18df07d612de10a6955573c81e7
floodliu <floodliu@gmail.com>
2016-08-31 15:19:02 +0800

    add a demo: php-tsdb-listTag.

--- tsdb/README.md
+++ tsdb/README.md
@@ -5,4 +5,4 @@
 | 写入数据点 || [php-tsdb-writeDatapoint](./php-tsdb-writeDatapoint) ||
 | 查询数据点 ||||
 | 获取metric列表 || [php-tsdb-listMetric](./php-tsdb-listMetric) ||
-| 获取标签 ||||
+| 获取标签 || [php-tsdb-listTag](./php-tsdb-listTag) ||
---
+++ tsdb/php-tsdb-listTag/listTag.php
@@ -0,0 +1,47 @@
+<?php
+
+require "../../authorization/auth.php";
+
+// 配置AK/SK和endpoint，生成认证字符串。
+
+$ak = "";  // AccessKeyID
+$sk = "";  // SecretAccessKey
+$endpoint = "";  // TSDB连接地址，如：{database}.tsdb.iot.gz.baidubce.com
+$metric = "";  // 只能获取与metric对应的tags
+
+$method = "GET";
+$uri = "/v1/metric/{$metric}/tag";
+$params = array();
+
+date_default_timezone_set("UTC");
+$timestamp = new \DateTime();
+$expirationInSeconds = 3600;
+
+$authorization = generateAuthorization($ak, $sk, $method, $endpoint, $uri, $params, $timestamp, $expirationInSeconds);
+print("authorization: {$authorization}\n");
+
+// 构造HTTP请求的URL、Header。
+
+$url = "http://{$endpoint}{$uri}";
+$timeStr = $timestamp->format("Y-m-d\TH:i:s\Z");
+$head = array(
+    "Content-Type:application/json",
+    "Authorization:{$authorization}",
+    "x-bce-date:{$timeStr}"
+);
+
+// 发送HTTP请求，写入数据点。
+
+$curlp = curl_init();
+curl_setopt($curlp, CURLOPT_URL, $url);
+curl_setopt($curlp, CURLOPT_HTTPHEADER, $head);
+
+curl_setopt($curlp, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($curlp);
+$status = curl_getinfo($curlp, CURLINFO_HTTP_CODE);
+curl_close($curlp);
+
+print("status: {$status}\n");
+print("response: {$response}\n");
+
+?>
