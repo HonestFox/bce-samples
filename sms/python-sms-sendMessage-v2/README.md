a082b3e9533c5dc53ce21f7c1f85373bf9e13c86
floodliu <floodliu@gmail.com>
2016-11-23 12:19:23 +0800

    add a demo: python-sms-sendMessage-v2.

---
+++ sms/python-sms-sendMessage-v2/README.md
@@ -0,0 +1,17 @@
+# python-sms-sendMessage-v2
+
+## 用途：
+
+发送短信（接口版本：v2）。
+
+**注意：仅适用于Python 2.7**
+
+## 使用方法：
+
+* 第一步：在文件`sms_client_conf.py`中配置AK/SK。
+* 第二步：在文件`send_message.py`中配置签名ID、模板ID、模板参数、接收者的手机号码。
+* 第三步：执行文件`send_message.py`。
+
+---
+
+bce-python-sdk 下载链接：[https://cloud.baidu.com/doc/Developer/index.html](https://cloud.baidu.com/doc/Developer/index.html)
---
+++ sms/python-sms-sendMessage-v2/sms_client_conf.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 导入Python标准日志模块
+import logging
+
+# 从Python SDK导入SMS配置管理模块以及安全认证模块
+from baidubce.bce_client_configuration import BceClientConfiguration
+from baidubce.auth.bce_credentials import BceCredentials
+
+# 设置SmsClient的Host、AK、SK
+# 注意：如果是gz区域的BCC用户，host应该使用 sms.gz.baidubce.com
+#      如果是其他区域的BCC用户，或者非BCC用户，则使用 sms.bj.baidubce.com
+host = "sms.bj.baidubce.com"
+AK = ""
+SK = ""
+
+# 设置日志文件的句柄和日志级别
+logger = logging.getLogger("baidubce.services.sms.smsclient")
+fh = logging.FileHandler("sms_sample.log")
+fh.setLevel(logging.DEBUG)
+
+# 设置日志文件输出的顺序、结构和内容
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+fh.setFormatter(formatter)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+
+# 创建BceClientConfiguration
+config = BceClientConfiguration(credentials=BceCredentials(AK, SK), endpoint=host)
